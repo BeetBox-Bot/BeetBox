2f45a5873f4a84a6604fed38e4665ee2e64ec1a5
Andrew Fayed <andrew.fayed@gmail.com>
2023-01-05 18:55:37 -0700

    Ping slash command working!

--- utilities/deploy-commands.js
+++ utilities/deploy-commands.js
@@ -8,7 +8,7 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = require(`../commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
